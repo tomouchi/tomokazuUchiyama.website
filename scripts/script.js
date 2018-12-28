8e197f119648a1db41f7a2033d520f8d1af08afa
tomokazuUchiyama <wisteria-0004@m3.gyao.ne.jp>
2018-12-28 14:07:47 +0900

    tabを半角スペースに変換

--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,69 @@
 $(function () {
-	// TOP▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-	// INFO▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-	"use strict";
-	$.getJSON("info.json", function (data) {
+    // TOP▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+    // INFO▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+    "use strict";
+    $.getJSON("info.json", function (data) {
 
-		var contentCount = data.content.length - 1;
-		var technicCount = data.technic.length - 1;
-		var fileNameRegex = /\index.html$/m;
-		if (fileNameRegex.test(location.href)) {
-			contentCount = data.content.length > 2 ? 2 : contentCount;
-			technicCount = data.technic.length > 2 ? 2 : technicCount;
-		}
+        var contentCount = data.content.length - 1;
+        var technicCount = data.technic.length - 1;
+        var fileNameRegex = /\index.html$/m;
+        if (fileNameRegex.test(location.href)) {
+            contentCount = data.content.length > 2 ? 2 : contentCount;
+            technicCount = data.technic.length > 2 ? 2 : technicCount;
+        }
 
-		$(".info__row__ele>dl").remove();
-		var i;
-		for (i = 0; i <= contentCount; i++) {
-			$("#content").after("<dl><dt>" + data.content[i][0] + "</dt><dd>" + data.content[i][1] + "</dd></dl>");
-		}
+        $(".info__row__ele>dl").remove();
+        var i;
+        for (i = 0; i <= contentCount; i++) {
+            $("#content").after("<dl><dt>" + data.content[i][0] + "</dt><dd>" + data.content[i][1] + "</dd></dl>");
+        }
 
-		for (i = 0; i <= technicCount; i++) {
-			$("#technic").after("<dl><dt>" + data.technic[i][0] + "</dt><dd>" + data.technic[i][1] + "</dd></dl>");
-		}
-	});
-	// INFO△△△△△△△△△△△△△△△△△△△△
+        for (i = 0; i <= technicCount; i++) {
+            $("#technic").after("<dl><dt>" + data.technic[i][0] + "</dt><dd>" + data.technic[i][1] + "</dd></dl>");
+        }
+    });
+    // INFO△△△△△△△△△△△△△△△△△△△△
 
 
 
-	// TOP△△△△△△△△△△△△△△△△△△△△
+    // TOP△△△△△△△△△△△△△△△△△△△△
 
-	// MUSIC▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-	$(".song__link > a").click(function () {
+    // MUSIC▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+    $(".song__link > a").click(function () {
 
-		$(".song__lyrics:not(:animated)").slideUp("slow");
+        $(".song__lyrics:not(:animated)").slideUp("slow");
 
-		var song__lyrics;
-		if ($(this).parent().parent().find(".song__lyrics").length === 0) {
+        var song__lyrics;
+        if ($(this).parent().parent().find(".song__lyrics").length === 0) {
 
-			$(this).closest(".songListEle").append('<div class="song__lyrics"></div>');
+            $(this).closest(".songListEle").append('<div class="song__lyrics"></div>');
 
-			song__lyrics = $($(this).parent().parent().find(".song__lyrics"));
-			song__lyrics.load("./" + $(this).attr("href") + " .lyrics", function () {
-				song__lyrics.hide().slideDown("slow");
-			});
+            song__lyrics = $($(this).parent().parent().find(".song__lyrics"));
+            song__lyrics.load("./" + $(this).attr("href") + " .lyrics", function () {
+                song__lyrics.hide().slideDown("slow");
+            });
 
-		} else {
+        } else {
 
-			song__lyrics = $($(this).parent().parent().find(".song__lyrics"));
-			if (song__lyrics.css("display") === "none") {
-				song__lyrics.slideDown("slow");
-			} else {
-				song__lyrics.slideUp("slow");
-			}
+            song__lyrics = $($(this).parent().parent().find(".song__lyrics"));
+            if (song__lyrics.css("display") === "none") {
+                song__lyrics.slideDown("slow");
+            } else {
+                song__lyrics.slideUp("slow");
+            }
 
-		}
+        }
 
-		return false;
+        return false;
 
-	});
-	// MUSIC△△△△△△△△△△△△△△△△△△△△
+    });
+    // MUSIC△△△△△△△△△△△△△△△△△△△△
 
-	// mainVisual▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-	$('.flexslider').flexslider({
-		directionNav: false,
-		animationSpeed:3000
-	});
-	// mainVisual△△△△△△△△△△△△△△△△△△△△
+    // mainVisual▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+    $('.flexslider').flexslider({
+        directionNav: false,
+        animationSpeed:3000
+    });
+    // mainVisual△△△△△△△△△△△△△△△△△△△△
 
 });
